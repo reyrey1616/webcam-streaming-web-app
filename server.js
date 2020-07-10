f6fe6650de695d162b0d616ba78acc6fa2f1d4df
yobbad16 <dabboyrey@gmail.com>
2020-07-11 02:57:09 +0800

    Setup the open cv to send the images through socket

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const path = require('path');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const cv = require('opencv4nodejs');
+
+const wCap = new cv.VideoCapture(0);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 setInterval(() => {
-  io.emit('image', 'somedata');
-}, 1000);
+  const frame = wCap.read();
+  const image = cv.imencode('.jpg', frame).toString('base64');
+
+  io.emit('image', image);
+}, 10000);
 
 server.listen(5000, () => {
   console.log('App listening on port 5000!');
